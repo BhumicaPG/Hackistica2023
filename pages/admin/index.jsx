d1858fca2c2f8180081b205cba8371ac39fdb4e1
Ayushi Agarwal <agarwalayushi2102@gmail.com>
2023-01-17 22:35:44 +0530

    Added delete menu option

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -65,7 +65,7 @@ const Index = ({ orders, products }) => {
                 </td>
                 <td>{product._id.slice(0, 5)}...</td>
                 <td>{product.title}</td>
-                <td>₹{product.prices[0]}</td>
+                <td>₹{product.prices}</td>
                 <td>
                   <button className={styles.button}>Edit</button>
                   <button
