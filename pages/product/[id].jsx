da79ca4903a05d483b5c0694c59bb30cd14d703c
Ayushi Agarwal <agarwalayushi2102@gmail.com>
2023-02-16 05:06:06 +0530

    Fixed base url

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -69,7 +69,7 @@ const Product = ({ pizza }) => {
 
 export const getServerSideProps = async ({ params }) => {
   const res = await axios.get(
-    `http://localhost:3000/api/products/${params.id}`
+    `${BASE_URL}/api/products/${params.id}`
   );
   return {
     props: {
